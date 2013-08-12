4a645c25c277d341febcc8b34897dd6751cf84f2
apaz89 <allen.paz@unitec.edu>
2013-08-12 16:56:28 -0600

    OTHER
    DFDF

--- MiniDropbox.Web/Models/PaquetesPremiumModel.cs
+++ MiniDropbox.Web/Models/PaquetesPremiumModel.cs
@@ -9,13 +9,11 @@ namespace MiniDropbox.Web.Models
     public class PaquetesPremiumModel
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long and maximum {1} characters long",
-            MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long and maximum {1} characters long",MinimumLength = 6)]
         [Display(Name = "Nombre del Paquete")]
         public string NombrePaquete { get; set; }
 
-        [StringLength(200, ErrorMessage = "The {0} must be at least {2} characters long and maximum {1} characters long",
-            MinimumLength = 25)]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} characters long and maximum {1} characters long",MinimumLength = 25)]
         [Display(Name = "Descripcion")]
         [DataType(DataType.MultilineText)]
         public string Descripcion { get; set; }
